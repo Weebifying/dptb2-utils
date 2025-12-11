96179bdf912a8bd57c8df376fded81a9b7d7aa67
Weebify <weebifylmao123@gmail.com>
2025-12-11 13:53:02 +0700

    fix: forgot to implement pitch stuff in notification...

--- src/main/java/weebify/dptb2utils/gui/widget/NotificationToast.java
+++ src/main/java/weebify/dptb2utils/gui/widget/NotificationToast.java
@@ -40,7 +40,7 @@ public class NotificationToast implements Toast {
         this(title, description, color, sfx, 1.0f, 1.0f);
     }
 
-    public NotificationToast(String title, String description, int color, @Nullable SoundEvent sfx, float volume, float pitch) {
+    public NotificationToast(String title, String description, int color, @Nullable SoundEvent sfx, float pitch, float volume) {
         this.title = title;
         this.description = description;
         this.color = color;
@@ -68,7 +68,7 @@ public class NotificationToast implements Toast {
         if (!this.soundPlayed && time > 0) {
             this.soundPlayed = true;
             if (this.sfx != null) {
-                manager.getClient().getSoundManager().play(PositionedSoundInstance.master(this.sfx, 1, 1));
+                manager.getClient().getSoundManager().play(PositionedSoundInstance.master(this.sfx, this.pitch, this.volume));
             }
         }
 
